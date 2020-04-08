07f233ce41fca72e0d775ecc5acca5aa7d414493
Kaká Cordovil <kakacordovil@gmail.com>
2020-04-08 16:08:28 +0100

    erro 404 resolved

--- src/main/java/org/academiadecodigo/javabank/persistence/dao/jpa/JpaRecipientDao.java
+++ src/main/java/org/academiadecodigo/javabank/persistence/dao/jpa/JpaRecipientDao.java
@@ -2,10 +2,12 @@ package org.academiadecodigo.javabank.persistence.dao.jpa;
 
 import org.academiadecodigo.javabank.persistence.model.Recipient;
 import org.academiadecodigo.javabank.persistence.dao.RecipientDao;
+import org.springframework.stereotype.Repository;
 
 /**
  * A JPA {@link RecipientDao} implementation
  */
+@Repository
 public class JpaRecipientDao extends GenericJpaDao<Recipient> implements RecipientDao {
 
     /**
